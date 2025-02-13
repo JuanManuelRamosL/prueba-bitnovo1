9f995ab7d8c0c1469a4c3aa05ea93b6de40ea548
juanmanuel <juanmanuelramoslencina@gmail.com>
2025-02-12 21:23:37 -0300

    estilos cometados

--- components/criptoSelector.jsx
+++ components/criptoSelector.jsx
@@ -1,7 +1,7 @@
 "use client";
 import { useState, useEffect } from "react";
 import { getCurrencies } from "../utils/api";
-import "./CryptoSelector.css"; // Importamos los estilos
+//import "./CryptoSelector.css"; // Importamos los estilos
 
 export default function CryptoSelector({ onSelect, amount }) {
   const [currencies, setCurrencies] = useState([]);
--- pages/success.jsx
+++ pages/success.jsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import "../styles/success.css"; // Importa el archivo CSS
+//import "../styles/success.css"; // Importa el archivo CSS
 
 export default function Success() {
   return (
